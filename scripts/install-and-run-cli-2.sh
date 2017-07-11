d99fc6b6c473b1867fef4bfcd0243f0f193ea659
Simon Davies <sdavies@microsoft.com>
2017-07-11 23:59:08 +0100

    updated script to  prevent prompting

--- scripts/install-and-run-cli-2.sh
+++ scripts/install-and-run-cli-2.sh
@@ -41,14 +41,14 @@ done
 
 # Install Docker and then run docker image with cli
 
-sudo apt-get update
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y update
+sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get -y update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 sudo docker run -v `pwd`:/scripts -network='host' \
 -e SUBSCRIPITON_ID=${subscription_id} \
 -e STORAGE_ACCOUNT=${storage_account} \
